864dfe805e74e0af423baea04133a863ff824220
Kapil Jugnu <kr_kapil@hotmail.com>
2025-02-26 16:49:47 +0530

    used custom pipe temperature

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -8,7 +8,7 @@ export class TemperaturePipe implements PipeTransform {
   transform(
     value: string | number | null,
     inputType: 'cel' | 'fah',
-    outputType: 'cel' | 'fah'
+    outputType?: 'cel' | 'fah'
   ) {
     if(!value) {
         return value;
